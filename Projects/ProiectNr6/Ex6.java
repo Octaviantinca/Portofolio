44c329092222bd782f8338c67222d33eaae07d54
Octaviantinca <octavian.tinca@yahoo.com>
2022-08-26 16:30:46 +0300

    Add Ex6

---
+++ Projects/ProiectNr6/Ex6.java
@@ -0,0 +1,34 @@
+package ProiectNr6;
+import java.util.*;
+
+public class Ex6 {
+
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+        Set<Integer> set = new TreeSet<>();
+        int n = s.nextInt();
+
+        for(int i = 0; i < n; i++){
+            set.add(s.nextInt());
+        }
+        int numarulDinColectie;
+
+        while(true){
+            System.out.println("Introduceti un numar din colectie: ");
+            numarulDinColectie = s.nextInt();
+            if(!set.contains(numarulDinColectie)) {
+                System.out.println("Numarul nu este din colectie, introduceti un numar din colectie");
+            } else {
+                break;
+            }
+        }
+        for(Integer i: set){
+            if(numarulDinColectie < i){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println("Nu exista un element in colectie care sa fie mai mare decat numarul dat!");
+    }
+}
